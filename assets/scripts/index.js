bae5c48a8c4be6dc71611652d83d779384702de3
“newayliu1” <newayliu1@gmail.com>
2017-01-24 11:48:23 -0500

    Add bootstrap modal
    - added the code in index.html and add handle in index.js

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,6 +5,10 @@ const config = require('./config');
 
 $(() => {
   setAPIOrigin(location, config);
+  $("#save_change").on("click", () => {
+    const input = $('#exampleInputEmail1').val();
+    console.log(input);
+  });
 });
 
 // use require with a reference to bundle the file and use it in this file
